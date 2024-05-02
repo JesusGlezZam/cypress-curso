06869d1043acb250f4e64627dde88586cac00dcd
LAPTOP-L7H0H0EL\glez_ <Jesus_Glez>
2024-05-02 14:17:49 -0600

    Agregando el reporte .html

--- cypress.config.js
+++ cypress.config.js
@@ -7,8 +7,16 @@ module.exports = defineConfig({
       require('@cypress/grep/src/plugin')(config);
       return config;
     },
+      // Añadir la configuración del reporter mochawesome aquí
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'cypress/reports/mocha',
+        overwrite: false,
+        html: true,
+        json: true
+      },
+      chromeWebSecurity: false,
   },
-  chromeWebSecurity: false,
   projectId: "k439nf",
   "experimentalStudio": true   //requerido para el Record
  // projectId: "8c1ca87c-23d6-47b3-8c94-2e6206c662b9",
